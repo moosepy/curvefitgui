05b6e1cdfa6b2223aa0f9403455e0539b4b31587
HK <kangerdev@icloud.com>
2021-06-23 10:44:37 +0200

    minor

---
+++ curvefitgui/__init__.py
@@ -0,0 +1,5 @@
+
+from .curvefitgui import curve_fit_gui
+from .curvefitgui import linear_fit_gui
+
+__version__ = '1.0.0'
--- setup.py
+++ setup.py
@@ -21,9 +21,8 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
     ],
-    python_requires='>=3.6, <4',
+    python_requires='>=3.7, <4',
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     install_requires=[
